08f496f3319723aeebbc7b83b7ee292fecb71213
saksham399 <kaushi29@purdue.edu>
2025-07-03 13:38:55 +0530

    Starting to render templates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 
 const router = require('./routes/flightRoutes');
@@ -7,6 +8,18 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`);
+
+
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
